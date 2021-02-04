f77e80c4da3d67032120a690c98744d342f6c18a
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2021-02-04 12:36:29 +0700

    Initial commit

---
+++ app/src/main/java/com/gzeinnumer/mylibdialogfragmentexample/CustomMyLibDialog.java
@@ -0,0 +1,44 @@
+package com.gzeinnumer.mylibdialogfragmentexample;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.gzeinnumer.mylibdialogfragment.MyLibDialog;
+
+public class CustomMyLibDialog extends MyLibDialog {
+
+    public static CustomMyLibDialog newInstance() {
+        return new CustomMyLibDialog();
+    }
+
+    public CustomMyLibDialog() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.dialog, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        LinearLayout _dialogCanvas = view.findViewById(R.id.dialog_canvas);
+
+        _dialogCanvas.setBackground(requireActivity().getResources().getDrawable(R.drawable.rounded_corner));
+
+        _dialogCanvas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getDialog().dismiss();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/gzeinnumer/mylibdialogfragmentexample/MainActivity.java
@@ -0,0 +1,30 @@
+package com.gzeinnumer.mylibdialogfragmentexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                Fragment previous = getSupportFragmentManager().findFragmentByTag(CustomMyLibDialog.TAG);
+                if(previous != null){
+                    transaction.remove(previous);
+                }
+                CustomMyLibDialog dialog = CustomMyLibDialog.newInstance();
+                dialog.show(transaction, CustomMyLibDialog.TAG);
+            }
+        });
+    }
+}
